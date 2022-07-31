00af2c99a0c4315ae636add87cad6d8e99ec32b7
ssajja31 <91031396+ssajja31@users.noreply.github.com>
2022-07-30 23:34:32 -0600

    Leap year

---
+++ Arrays/Modular Arithmatic/LeapYear.cs
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int solve(int A)
+    {
+        if (A % 400 == 0)
+        {
+            return 1;
+        }
+        else if (A % 4 == 0 && A % 100 != 0)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}
