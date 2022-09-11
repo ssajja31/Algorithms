3e3fd9e972bc051ca44fef00feecde3493efae02
ssajja31 <91031396+ssajja31@users.noreply.github.com>
2022-09-11 15:08:57 -0600

    SingleNumber

---
+++ Advanced/BitManipulations/SingleNumber.cs
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int numSetBits(int A)
+    {
+        int count = 0;
+        while (A != 0)
+        {
+            if ((A & 1) == 1)
+            {
+                count++;
+            }
+            A = A >> 1;
+        }
+
+        return count;
+    }
+}
