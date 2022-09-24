85a0809ea15cc31d0c7531beb960bfccc91166cf
ssajja31 <91031396+ssajja31@users.noreply.github.com>
2022-09-24 12:28:35 -0600

    AnotherSubSequenceProblem

---
+++ Advanced/Recursion/AnotherSubSequenceProblem.cs
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solve(int A)
+    {
+        if (A == 0)
+        {
+            return 1;
+        }
+        if (A == 1 || A == 2)
+        {
+            return A;
+        }
+
+        return solve(A - 1) + solve(A - 2) + solve(A - 3) + A;
+    }
+
+}
